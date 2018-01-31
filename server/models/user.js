d8a89d0953fd291b0c43ace74cbf90392619f950
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-01-30 22:11:22 -0600

    Added login route

--- server/models/user.js
+++ server/models/user.js
@@ -77,6 +77,24 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  let User = this;
+  return User.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 // .pre runs code before a given event
 UserSchema.pre('save', function(next) {
   let user = this;
--- server/server.js
+++ server/server.js
@@ -136,6 +136,19 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password)
+    .then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+      });
+    })
+    .catch((e) => {
+      res.status(400).send();
+    });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server up on ${process.env.PORT}`);
 });
